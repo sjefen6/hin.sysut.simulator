8e37916f130f0d51754d7815e2f658fdab4f2064
Vegard Langås <sjefen6@gmail.com>
2012-06-03 13:25:20 +0200

    UsagePattern is working

---
+++ src/org/hikst/Simulator/UsagePatternNotFoundException.java
@@ -0,0 +1,11 @@
+package org.hikst.Simulator;
+
+public class UsagePatternNotFoundException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -694531456940802354L;
+
+}
