64a8b9997050b950c874d260a258902ba7e1e81c
Vegard Langås <sjefen6@gmail.com>
2012-06-03 14:20:42 +0200

    Manuel merge førr Pål. Begynner å gå lei

---
+++ src/org/hikst/Simulator/TypeIdNotFoundException.java
@@ -0,0 +1,11 @@
+package org.hikst.Simulator;
+
+public class TypeIdNotFoundException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9092520965519121575L;
+	
+}
