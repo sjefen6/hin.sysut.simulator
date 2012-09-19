3153c9e88745c0209ae71f00dd979e7a97d25be7
Oxycoon <daniel.dimby@gmail.com>
2012-09-19 15:43:33 +0200

    More sunlight equations

--- src/org/hikst/Simulator/ImpactFactor.java
+++ src/org/hikst/Simulator/ImpactFactor.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
 
 /**
  * TODO:
@@ -15,8 +21,16 @@ public class ImpactFactor
 	private int type_id;
 	private String content;
 	
+	private Calendar calendar;
+	
+	//Variables for sun related factors
+	private Double lengthOfDay;
+	private boolean sunlight;
+	
 	public ImpactFactor(int id)
 	{
+		calendar = new GregorianCalendar();
+		
 		Connection connection = Settings.getDBC();
 		
 		try
@@ -37,6 +51,62 @@ public class ImpactFactor
 			ex.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Calculates the number of daylight hours on the specified 
+	 * day defined in the given time and at the given geographical latitude.
+	 * 
+	 * @param latitude Latitude in degrees
+	 * @param time Date object with time data of the desired time.
+	 */
+	private void setLengthOfDay(double latitude, Date time)
+	{
+		calendar.setTime(time);
+		int date = calendar.get(Calendar.DAY_OF_YEAR);
+		
+		double p = Math.asin(0.39795*Math.cos(0.2163108 + 2*Math.atan(0.9671396*Math.tan(0.00860*(date-186)))));
+		
+		lengthOfDay = 24 - (24/Math.PI)*Math.acos((Math.sin(0.8333*Math.PI/180)+
+								Math.sin(latitude*Math.PI/180)*Math.sin(p))
+								/(Math.cos(latitude*Math.PI/180)*Math.cos(p)));		
+	}
+	
+	/**
+	 * @return The length of this impactFactor's day in hours, if the length is not specified, returns Not a Number.
+	 */
+	public Double getHoursOfSunLight()
+	{		
+		if (lengthOfDay != null)
+			return lengthOfDay;
+		else
+			return Double.NaN;
+	}
+	
+	public void setSunlight(Date time)
+	{
+		calendar.setTime(time);
+		double tempHours = getHoursOfSunLight();
+		double tempTime = time.getHours() + TimeUnit.MINUTES.toHours(time.getMinutes());
+		
+		if(tempHours != Double.NaN)
+		{
+			if (tempTime > (12 - (tempHours/2)) && tempTime < (12 + tempHours/2))
+			{
+				sunlight = false;
+			}
+			else
+				sunlight = false;
+		}
+		else
+			sunlight = false;
+	}
+	
+	public boolean getSunlight()
+	{
+		return sunlight;
+	}
+	
+	
 	
 	/**
 	 *	TODO:
--- src/org/hikst/Simulator/UsagePattern.java
+++ src/org/hikst/Simulator/UsagePattern.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.text.DateFormatter;
 
@@ -16,7 +17,6 @@ public class UsagePattern {
 	private String name;
 	private int[] pattern = new int[24];
 	private boolean actual;
-	private boolean sun;
 	private Calendar calendar;
 	
 	//TODO:
@@ -53,20 +53,6 @@ public class UsagePattern {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public double getHoursOfSunLight(double latitude)
-	{
-		int date = calendar.get(Calendar.DAY_OF_YEAR);
-		
-		double p = Math.asin(0.39795*Math.cos(0.2163108 + 2*Math.atan(0.9671396*Math.tan(0.00860*(date-186)))));
-		
-		
-		double lengthOfDay = 24 - (24/Math.PI)*Math.acos((Math.sin(0.8333*Math.PI/180)+
-								Math.sin(latitude*Math.PI/180)*Math.sin(p))
-								/(Math.cos(latitude*Math.PI/180)*Math.cos(p)));		
-		
-		return lengthOfDay;
 	}
 
 	public int getId() {
